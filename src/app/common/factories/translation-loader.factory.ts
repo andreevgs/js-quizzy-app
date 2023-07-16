4b985e27ea047fad5fa1947e6fe75b8511e35aa7
Gleb <unicode256@yandex.by>
2023-07-16 21:26:01 +0300

    feat: add base url checking to translation-loader.factory.ts

--- src/app/common/factories/translation-loader.factory.ts
+++ src/app/common/factories/translation-loader.factory.ts
@@ -4,5 +4,7 @@ import {TranslateHttpLoader} from "@ngx-translate/http-loader";
 export function HttpLoaderFactory(http: HttpClient) {
   const baseUrl = document.baseURI;
   console.log('Current base URL:', baseUrl);
-  return new TranslateHttpLoader(http);
+  return baseUrl.includes('github.io') ?
+    new TranslateHttpLoader(http, '/js-quizzy-app/assets/i18n/') :
+    new TranslateHttpLoader(http);
 }
