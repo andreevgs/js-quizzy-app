3170e3a23babf28cd044f3282c01de7bee952fa4
Gleb <unicode256@yandex.by>
2023-09-07 17:21:57 +0300

    feat: dynamic height assignment

--- src/main.ts
+++ src/main.ts
@@ -7,3 +7,11 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+const appHeight = () => {
+  const doc = document.documentElement
+  doc.style.setProperty('--app-height', `${window.innerHeight}px`);
+  console.log('resized');
+}
+window.addEventListener('resize', appHeight);
+appHeight();
